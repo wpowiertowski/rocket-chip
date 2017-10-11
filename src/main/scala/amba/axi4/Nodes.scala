285b04a275bfe8789bc77af41f8ded860abd70cb
Wesley W. Terpstra <wesley@sifive.com>
2017-10-11 14:55:28 -0700

    axi4: implement a diplomatic AXI4 clock crossing

--- src/main/scala/amba/axi4/Nodes.scala
+++ src/main/scala/amba/axi4/Nodes.scala
@@ -45,3 +45,34 @@ case class AXI4BlindInputNode(portParams: Seq[AXI4MasterPortParameters]) extends
 
 case class AXI4InternalOutputNode(portParams: Seq[AXI4SlavePortParameters]) extends InternalOutputNode(AXI4Imp)(portParams)
 case class AXI4InternalInputNode(portParams: Seq[AXI4MasterPortParameters]) extends InternalInputNode(AXI4Imp)(portParams)
+
+object AXI4AsyncImp extends NodeImp[AXI4AsyncMasterPortParameters, AXI4AsyncSlavePortParameters, AXI4AsyncEdgeParameters, AXI4AsyncEdgeParameters, AXI4AsyncBundle]
+{
+  def edgeO(pd: AXI4AsyncMasterPortParameters, pu: AXI4AsyncSlavePortParameters) = AXI4AsyncEdgeParameters(pd, pu)
+  def edgeI(pd: AXI4AsyncMasterPortParameters, pu: AXI4AsyncSlavePortParameters) = AXI4AsyncEdgeParameters(pd, pu)
+
+  def bundleO(ei: AXI4AsyncEdgeParameters) = new AXI4AsyncBundle(ei.bundle)
+  def bundleI(eo: AXI4AsyncEdgeParameters) = new AXI4AsyncBundle(eo.bundle)
+
+  def colour = "#ff0000" // red
+  override def labelI(ei: AXI4AsyncEdgeParameters) = ei.slave.depth.toString
+  override def labelO(eo: AXI4AsyncEdgeParameters) = eo.slave.depth.toString
+
+  override def mixO(pd: AXI4AsyncMasterPortParameters, node: OutwardNode[AXI4AsyncMasterPortParameters, AXI4AsyncSlavePortParameters, AXI4AsyncBundle]): AXI4AsyncMasterPortParameters  =
+   pd.copy(base = pd.base.copy(masters = pd.base.masters.map  { c => c.copy (nodePath = node +: c.nodePath) }))
+  override def mixI(pu: AXI4AsyncSlavePortParameters, node: InwardNode[AXI4AsyncMasterPortParameters, AXI4AsyncSlavePortParameters, AXI4AsyncBundle]): AXI4AsyncSlavePortParameters =
+   pu.copy(base = pu.base.copy(slaves  = pu.base.slaves.map { m => m.copy (nodePath = node +: m.nodePath) }))
+}
+
+case class AXI4AsyncSourceNode(sync: Int)
+  extends MixedAdapterNode(AXI4Imp, AXI4AsyncImp)(
+    dFn = { p => AXI4AsyncMasterPortParameters(p) },
+    uFn = { p => p.base.copy(minLatency = sync+1) }) // discard cycles in other clock domain
+
+case class AXI4AsyncSinkNode(depth: Int, sync: Int)
+  extends MixedAdapterNode(AXI4AsyncImp, AXI4Imp)(
+    dFn = { p => p.base },
+    uFn = { p => AXI4AsyncSlavePortParameters(depth, p) })
+
+case class AXI4AsyncInputNode() extends InputNode(AXI4AsyncImp)
+case class AXI4AsyncOutputNode() extends InputNode(AXI4AsyncImp)
